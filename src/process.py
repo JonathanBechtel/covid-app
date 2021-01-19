67b78c4e2626d15fb24eda4766c0bed724a1796f
Jonathan Bechtel <jonathanbechtel@gmail.com>
2021-01-19 03:37:53 +0000

    updated web task

--- src/process.py
+++ src/process.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-
 """
 This file is designed to organize all the different subroutines needed to make the application work
 on a daily basis
 """
 import pickle
-from utils import build_prophet_preds, add_to_db
+from utils import add_to_db
+from model import build_prophet_preds
+import os
+
+# setup absolute file directory paths
+script_directory = os.path.dirname(os.path.abspath(__file__))
+data_file        = os.path.join(script_directory, "info.pkl")
 
 # load in db info
-with open('info.pkl', 'rb') as info:
+with open(data_file, 'rb') as info:
     db_info = pickle.load(info)
-    
-connection_string = f"{db_info['driver']://{db_info['username']}:{db_info['password']}@{db_info['host']}/{db_info['database']}"
+
+connection_string = f"{db_info['driver']}://{db_info['username']}:{db_info['password']}@{db_info['host']}:{db_info['port']}/{db_info['database']}"
 
 
 data_url          = 'https://api.covidtracking.com/v1/states/daily.csv'
